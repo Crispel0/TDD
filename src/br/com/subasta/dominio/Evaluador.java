6dfa3df539251a840651d98dbc5769f13aa1bfff
Crispelo <97807364+Crispel0@users.noreply.github.com>
2022-12-09 08:21:05 -0500

    agregando prueba caso de uso mayores de 5 cifras

--- src/br/com/subasta/dominio/Evaluador.java
+++ src/br/com/subasta/dominio/Evaluador.java
@@ -1,9 +1,16 @@
 package br.com.subasta.dominio;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Evaluador {
 	
 	private double mayorQueTodos = Double.NEGATIVE_INFINITY;
 	private double menorQueTodos = Double.POSITIVE_INFINITY;
+	private List<Oferta> ofertasMayores = new ArrayList<Oferta>(); //Crea ArrayList de Ofertas//
+	
 	
 	public void evalua(Subasta subasta) {
 		for (Oferta oferta : subasta.getOfertas()) {
@@ -14,9 +21,25 @@ public class Evaluador {
 					menorQueTodos = oferta.getValor();
 				}
 			}
+		// asigna todas las ofertas por un ArrayList a ofertasMayores
+		ofertasMayores = new ArrayList<>(subasta.getOfertas());
+		//sorteo de las ofertas y comparacion entre una oferta y otra//
+		Collections.sort(ofertasMayores , new Comparator<Oferta>() {
+			@Override
+			public int compare(Oferta oferta1, Oferta oferta2) {
+				if(oferta1.getValor() < oferta2.getValor()) {
+					return -1; //si la primera oferta es mayor que la oferta 2 retorname -1//
+				}else if(oferta1.getValor() > oferta2.getValor()) {
+					return 1; //por el contrario retorname 1//
+				}
+				
+				return 0; //si son iguales retorname 0//			
+			}});
+																							
+		ofertasMayores = ofertasMayores.subList(0, 3); //toma el la primerOferta por indice//
 		}
 	
-
+	
 	public double getMayorQueTodos() {
 		return mayorQueTodos;
 	}
@@ -24,7 +47,12 @@ public class Evaluador {
 	public double getMenorQueTodos() {
 		return menorQueTodos;
 	}
-	
-	
 
+
+	public List<Oferta> getOfertasMayores() {
+		return ofertasMayores;
+	}	
+	
+	
+	
 }
--- src/br/com/subasta/dominio/TestJunitEvaluador.java
+++ src/br/com/subasta/dominio/TestJunitEvaluador.java
@@ -23,7 +23,6 @@ public class TestJunitEvaluador {
 		
 		Evaluador evaluador = new Evaluador();
 		evaluador.evalua(subasta);
-		
 		System.out.println(evaluador.getMayorQueTodos());
 		System.out.println(evaluador.getMenorQueTodos());
 		
@@ -32,5 +31,88 @@ public class TestJunitEvaluador {
 		
 	}
 	
-
+		@Test
+		public void deberiaRetornarElMayoresValores5Cifras() {
+			
+			Usuario diego = new Usuario(1, "Diego");
+			Usuario alexiz = new Usuario(2, "Alexiz");
+			Usuario daniel = new Usuario(3, "Daniel");
+			
+			Oferta oferta1 = new Oferta(diego, 34021.00);
+			Oferta oferta2 = new Oferta(alexiz, 56024.00);
+			Oferta oferta3 = new Oferta(daniel, 77041.00);
+			
+			Subasta subasta = new Subasta("Auto");
+			subasta.propone(oferta1);
+			subasta.propone(oferta2);
+			subasta.propone(oferta3);
+			
+			Evaluador evaluador = new Evaluador();
+			evaluador.evalua(subasta);
+			System.out.println(evaluador.getMayorQueTodos());
+			System.out.println(evaluador.getMenorQueTodos());
+																//param. Operador Delta-Redondeo a entero un decimal//
+			Assert.assertEquals(77041, evaluador.getMayorQueTodos(), 0.00001);
+			Assert.assertEquals(34021, evaluador.getMenorQueTodos(), 0.00001);
+			
+	}
+		
+		@Test
+		public void MayoresTresOfertas() {
+			Usuario Alejandria = new Usuario("Alejandria");
+			Usuario Alejandro = new Usuario("Alejandro");
+			Usuario Alejo = new Usuario("Alejo");
+			Usuario Martes = new Usuario("Martes");
+			Usuario carla = new Usuario("Carla");
+			
+			Oferta ofertaMartes = new Oferta(Martes, 200000.00);
+			Oferta ofertaAlejandro = new Oferta(Alejandro, 300000.00);
+			Oferta ofertaAlejo = new Oferta(Alejo, 400000.00);
+			Oferta ofertaAlejandria = new Oferta(Alejandria, 500000.00);
+			Oferta ofertaCarla = new Oferta(carla, 600000.00);
+			
+			Subasta audifonos = new Subasta("Audifonos inalambricos");
+			audifonos.propone(ofertaAlejandria);
+			audifonos.propone(ofertaAlejo);
+			audifonos.propone(ofertaMartes);
+			audifonos.propone(ofertaAlejandro);
+			audifonos.propone(ofertaCarla);
+			
+			
+			
+			Evaluador evaluador = new Evaluador();
+			evaluador.evalua(audifonos);
+			
+			Assert.assertEquals(evaluador.getOfertasMayores().size(), 3);
+			Assert.assertEquals(evaluador.getOfertasMayores().get(1).getValor(), 300000.00);
+			Assert.assertEquals(evaluador.getOfertasMayores().get(2).getValor(), 400000.00);
+			Assert.assertEquals(evaluador.getOfertasMayores().get(3).getValor(), 500000.00);
+		}
+		@Test
+		public void ComprenderValoresOfertaAleatoriamente() {
+			
+			Usuario Alejandria = new Usuario("Alejandria");
+			Usuario Alejandro = new Usuario("Alejandro");
+			Usuario Alejo = new Usuario("Alejo");
+			Usuario Martes = new Usuario("Martes");
+			Usuario carla = new Usuario("Carla");
+			
+			Oferta ofertaMartes = new Oferta(Martes, 32457.00);
+			Oferta ofertaAlejandro = new Oferta(Alejandro, 15478.00);
+			Oferta ofertaAlejo = new Oferta(Alejo, 45478.00);
+			Oferta ofertaAlejandria = new Oferta(Alejandria, 54712.00);
+			Oferta ofertaCarla = new Oferta(carla, 32564.00);
+			
+			Subasta audifonos = new Subasta("Audifonos inalambricos");
+			audifonos.propone(ofertaAlejandria);
+			audifonos.propone(ofertaAlejo);
+			audifonos.propone(ofertaMartes);
+			audifonos.propone(ofertaAlejandro);
+			audifonos.propone(ofertaCarla);
+			
+			Evaluador evaluador = new Evaluador();
+					evaluador.evalua(audifonos);
+			
+			Assert.assertEquals(evaluador.getOfertasMayores() < , false);
+		}
 }
