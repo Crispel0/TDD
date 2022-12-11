75d87f5499f11e9ce36ee31c48d6e47d7460e453
Crispelo <97807364+Crispel0@users.noreply.github.com>
2022-12-11 18:09:59 -0500

    Refactorizando clase TestJunitEvaluador con Pattern Builder

--- src/col/com/subasta/dominio/TestJunit/TestJunitEvaluador.java
+++ src/col/com/subasta/dominio/TestJunit/TestJunitEvaluador.java
@@ -16,7 +16,6 @@ public class TestJunitEvaluador {
 	
 	@Before
 	public void  prepararEscenario() {
-		
 		this.subasta = subasta = new ConstruirSubasta().para("Bicicleta").oferta(new Usuario("diego"), 250000).
 				oferta(new Usuario("Alexis"), 300000).
 				oferta(new Usuario("daniel"), 450000).
@@ -32,8 +31,8 @@ public class TestJunitEvaluador {
 		System.out.println(evaluador.getMenorQueTodos());
 		
 		//El parametro delta es usado para redondear el valor double 0.00001
-		Assert.assertEquals(2456841, evaluador.getMayorQueTodos(),0.00001);
-		Assert.assertEquals(1457612, evaluador.getMenorQueTodos(),0.00001);
+		Assert.assertEquals(450000, evaluador.getMayorQueTodos(),0.00001);
+		Assert.assertEquals(250000, evaluador.getMenorQueTodos(),0.00001);
 		
 	}
 	
@@ -45,9 +44,29 @@ public class TestJunitEvaluador {
 			System.out.println(evaluador.getMayorQueTodos());
 			System.out.println(evaluador.getMenorQueTodos());
 																//param. Operador Delta-Redondeo a entero un decimal//
-			Assert.assertEquals(2456841, evaluador.getMayorQueTodos(), 0.00001);
-			Assert.assertEquals(1457612, evaluador.getMenorQueTodos(), 0.00001);
+			Assert.assertEquals(450000, evaluador.getMayorQueTodos(), 0.00001);
+			Assert.assertEquals(250000, evaluador.getMenorQueTodos(), 0.00001);
 			
 	}
-
-}
+		
+		
+		@Test
+		public void MayoresTresOfertas() {
+			
+			this.subasta = subasta = new ConstruirSubasta().para("Bicicleta").oferta(new Usuario("diego"), 250000).
+					oferta(new Usuario("Alexis"), 300000).
+					oferta(new Usuario("daniel"), 450000).
+					oferta(new Usuario("Martes"), 500000).
+					oferta(new Usuario("Carla"), 600000).
+					Creador();
+			
+			Evaluador evaluador = new Evaluador();
+			evaluador.evalua(subasta);
+			
+			Assert.assertEquals(evaluador.getOfertasMayores().size(), 3);
+			Assert.assertEquals(evaluador.getOfertasMayores().get(1).getValor(), 300000.00);
+			Assert.assertEquals(evaluador.getOfertasMayores().get(2).getValor(), 450000.00);
+			Assert.assertEquals(evaluador.getOfertasMayores().get(3).getValor(), 500000.00);
+		}
+		
+	}
--- src/col/com/subasta/dominio/TestJunit/TestJunitMayoresOfertas.java
+++ src/col/com/subasta/dominio/TestJunit/TestJunitMayoresOfertas.java
@@ -9,35 +9,4 @@ import col.com.subasta.dominio.Usuario;
 import junit.framework.Assert;
 
 public class TestJunitMayoresOfertas {
-	
-	@Test
-	public void MayoresTresOfertas() {
-		Usuario Alejandria = new Usuario(1,"Alejandria");
-		Usuario Alejandro = new Usuario(2,"Alejandro");
-		Usuario Alejo = new Usuario(3,"Alejo");
-		Usuario Martes = new Usuario(4,"Martes");
-		Usuario carla = new Usuario(5,"Carla");
-		
-		Oferta ofertaMartes = new Oferta(Martes, 200000.00);
-		Oferta ofertaAlejandro = new Oferta(Alejandro, 300000.00);
-		Oferta ofertaAlejo = new Oferta(Alejo, 400000.00);
-		Oferta ofertaAlejandria = new Oferta(Alejandria, 500000.00);
-		Oferta ofertaCarla = new Oferta(carla, 600000.00);
-		
-		Subasta audifonos = new Subasta("Audifonos inalambricos");
-		audifonos.propone(ofertaAlejandria);
-		audifonos.propone(ofertaAlejo);
-		audifonos.propone(ofertaMartes);
-		audifonos.propone(ofertaAlejandro);
-		audifonos.propone(ofertaCarla);
-		
-		Evaluador evaluador = new Evaluador();
-		evaluador.evalua(audifonos);
-		
-		Assert.assertEquals(evaluador.getOfertasMayores().size(), 3);
-		Assert.assertEquals(evaluador.getOfertasMayores().get(1).getValor(), 300000.00);
-		Assert.assertEquals(evaluador.getOfertasMayores().get(2).getValor(), 400000.00);
-		Assert.assertEquals(evaluador.getOfertasMayores().get(3).getValor(), 500000.00);
-	}
-	
-}
+
